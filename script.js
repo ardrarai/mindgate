f68387a292a5d6f98969d8bf9cbb49fb077b7103
oradora! <146644869+ardrarai@users.noreply.github.com>
2025-06-11 23:41:16 +0530

    ripple generation added!!!!

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const sendBtn = document.getElementById('sendBtn');
 const userInput = document.getElementById('userInput');
 const orb = document.querySelector('.crystal-orb');
+const rippleContainer = document.querySelector('.ripple-container');
 
 sendBtn.addEventListener('click', () => {
     const inputText = userInput.value.trim();
     if (inputText !== '') {
         activatePulse();
+        createRipple();
         userInput.value = '';
     }
 });
@@ -14,5 +16,15 @@ function activatePulse() {
     orb.classList.add('active');
     setTimeout(() => {
         orb.classList.remove('active');
+    }, 1000);
+}
+
+function createRipple() {
+    const ripple = document.createElement('div');
+    ripple.classList.add('ripple');
+    rippleContainer.appendChild(ripple);
+
+    setTimeout(() => {
+        ripple.remove();
     }, 1000);
 }
